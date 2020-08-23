95942bdd6379fed7be4811e87d95e11e6d48509f
KazuyoshiHidaka <kazuyoshi.first@gmail.com>
2020-08-23 13:11:02 +0000

    FontawesomeからGoogle Material Iconsに変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
 require('bootstrap');
-require('@fortawesome/fontawesome-free');
 require('./all_songs_list');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
